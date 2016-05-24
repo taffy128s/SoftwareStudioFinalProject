05d4b471fcd88bfd04a838f373ac21ebd463162d
LittleBird <ilms49898723@gmail.com>
2016-05-24 19:50:13 +0900

    use space for indent

--- src/server/Main.java
+++ src/server/Main.java
@@ -2,9 +2,9 @@ package server;
 
 public class Main {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         // TODO Auto-generated method stub
 
-	}
+    }
 
 }
