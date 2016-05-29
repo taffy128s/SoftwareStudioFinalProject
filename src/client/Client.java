66c0df41e4c9d7ba1bc5f700a45854f52d2acdf5
LittleBird <ilms49898723@gmail.com>
2016-05-30 01:08:35 +0900

    fix some small problems

--- src/client/Client.java
+++ src/client/Client.java
@@ -3,8 +3,7 @@ package client;
 public class Client {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-    	
+
     }
 
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,7 +1,6 @@
 package server;
 
 import java.awt.Dimension;
-import java.util.List;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,106 +11,116 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Date;
 
-import javax.swing.BoxLayout;
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+import javax.swing.*;
 
 @SuppressWarnings("serial")
 public class Server extends JFrame {
 
-	private int connectionCount;
-	private List<ConnectionThread> connections = new ArrayList<ConnectionThread>();
-	private ServerSocket servSocket;
-	private Date date = new Date();
-	private JTextArea textArea = new JTextArea();
-	
+    private int connectionCount;
+    private ArrayList<ConnectionThread> connections = new ArrayList<>();
+    private ServerSocket serverSocket;
+    private Date date = new Date();
+    private JTextArea textArea = new JTextArea();
+
     Server(int port) {
-    	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    	this.setSize(400, 300);
-    	this.setVisible(true);
-    	this.setResizable(false);
-    	this.setLocation(300, 300);
-    	
-    	this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.PAGE_AXIS));
-		this.textArea.setEditable(false);
-		this.setPreferredSize(new Dimension(400, 300));
-		JScrollPane scrollPane = new JScrollPane(this.textArea);
-		this.add(scrollPane);
-		
-		try {
-			servSocket = new ServerSocket(port);
-			StringBuilder builder = new StringBuilder(date.toString() + "\n");
-			builder.append("Server starts listening on port ").append(port).append(".\n");
-			textArea.append(builder.toString());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setSize(400, 300);
+        this.setVisible(true);
+        this.setResizable(false);
+        this.setLocation(300, 300);
+
+        this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.PAGE_AXIS));
+        this.textArea.setEditable(false);
+        this.setPreferredSize(new Dimension(400, 300));
+        JScrollPane scrollPane = new JScrollPane(this.textArea);
+        this.add(scrollPane);
+
+        try {
+            serverSocket = new ServerSocket(port);
+            textArea.append(date.toString() + "\n" + "Server starts listening on port " + port + ".\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    
+
     public void startAcceptConnection() {
-    	while (true) {
-    		try {
-    			Socket connSock = servSocket.accept();
-    			// Show message on server.
-    			textArea.append("Connection established.\n");
-				StringBuilder builder = new StringBuilder("Player ");
-				builder.append(connectionCount + 1).append("'s host name: ").append(connSock.getInetAddress().getHostName() + "\n");
-				textArea.append(builder.toString());
-				builder = new StringBuilder("Player ");
-				builder.append(connectionCount + 1).append("'s IP address: ").append(connSock.getInetAddress().getHostAddress() + "\n");
-				textArea.append(builder.toString());
-				// Create a new thread and add it to list.
-				ConnectionThread connThread = new ConnectionThread(connSock);
-				connThread.start();
-				connections.add(connThread);
-    		} catch (Exception e) {
-    			e.printStackTrace();
-    		}
-    	}
+        while (true) {
+            try {
+                Socket connSock = serverSocket.accept();
+                // Show message on server.
+                textArea.append("Connection established.\n");
+                StringBuilder builder = new StringBuilder("Player ");
+                builder.append(connectionCount + 1).append("'s host name: ").append(connSock.getInetAddress().getHostName()).append("\n");
+                textArea.append(builder.toString());
+                builder = new StringBuilder("Player ");
+                builder.append(connectionCount + 1).append("'s IP address: ").append(connSock.getInetAddress().getHostAddress()).append("\n");
+                textArea.append(builder.toString());
+                // Create a new thread and add it to list.
+                ConnectionThread connectionThread = new ConnectionThread(connSock);
+                connectionThread.start();
+                connections.add(connectionThread);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
-    
+
     public void broadCast(String string) {
-    	for (ConnectionThread thread : connections) {
-    		thread.sendMessage(string);
-    	}
+        for (ConnectionThread thread : connections) {
+            thread.sendMessage(string);
+        }
     }
 
     class ConnectionThread extends Thread {
-    	
-    	private Socket socket;
-    	private BufferedReader reader;
-    	private PrintWriter writer;
-    	
-    	public ConnectionThread(Socket socket) {
-    		this.socket = socket;
-    		try {
-				this.writer = new PrintWriter(new OutputStreamWriter(this.socket.getOutputStream()));
-				this.reader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-    	}
-    	
-    	public void sendMessage(String string) {
-    		writer.println(string);
-    		writer.flush();
-    	}
-    	
-    	public void run() {
-    		while (true) {
-    			try {
-					String data = reader.readLine();
-					textArea.append(data);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-    		}
-    	}
+
+        private Socket socket;
+        private BufferedReader reader;
+        private PrintWriter writer;
+
+        public ConnectionThread(Socket socket) {
+            this.socket = socket;
+            try {
+                this.writer = new PrintWriter(new OutputStreamWriter(this.socket.getOutputStream()));
+                this.reader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void sendMessage(String string) {
+            writer.println(string);
+            writer.flush();
+        }
+
+        public void run() {
+            while (true) {
+                try {
+                    String data = reader.readLine();
+                    if (data == null) {
+                        break;
+                    }
+                    textArea.append(data);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
     }
-    
+
     public static void main(String[] args) {
-    	Server server = new Server(6666);
-    	server.startAcceptConnection();
+        if (args.length != 1) {
+            System.out.println("missing <port> parameter");
+            return;
+        }
+        try {
+            int port = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            System.out.println(args[1] + " is not a valid number");
+            return;
+        }
+        Server server = new Server(Integer.valueOf(args[1]));
+        server.startAcceptConnection();
     }
+
 }
