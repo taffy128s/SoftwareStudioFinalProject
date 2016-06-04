9a5fe0941838cdf3e0c63431bad45896b46326ea
楊澤仁 <gcobs61905@gmail.com>
2016-06-04 15:29:13 +0800

    Some CARDS

---
+++ src/card/JinWuKu.java
@@ -0,0 +1,17 @@
+package card;
+
+/**
+ * Bumper Harvest 五穀豐登
+ */
+public class JinWuKu extends Card {
+	/**
+     * Default constructor
+     */
+    public JinWuKu() {
+        super(CardCategory.SKILL,
+              "Bumper Harvest",
+              "In your phase, you can use 'Cavalry Invasion' to all players. You show X cards(X = the number of players still alive), and from you according to the action order, every player choose one card and obtain it." ,
+              "jin_wuku.JPG");
+    }
+
+}
