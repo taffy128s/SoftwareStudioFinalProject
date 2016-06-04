1c6ac6e4a60fb5f750600fc9a3be85efd507f518
LittleBird <ilms49898723@gmail.com>
2016-06-05 02:08:47 +0900

    let server can create a lot of games, add ServerUtility

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,25 +1,13 @@
 package server;
 
 import java.awt.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.TreeMap;
 
 import javax.swing.*;
-
-
-import card.Card;
-import card.CardID;
-import card.CardStack;
 
 /**
  * Server
@@ -31,77 +19,19 @@ public class Server extends JFrame {
     private static final int WINDOW_HEIGHT = 720;
 
     private int connectionCount;
-    private Date date = new Date();
     private ServerSocket serverSocket;
-    private ArrayList<ConnectionThread> connections = new ArrayList<>();
-    private HashMap<ConnectionThread, String> threadToUsername = new HashMap<>();
-    private TreeMap<String, String> usernameToIntent = new TreeMap<>();
+    private ArrayList<Socket> sockets = new ArrayList<>();
+    private ArrayList<ServerUtility> games = new ArrayList<>();
+
     private JTextArea textArea = new JTextArea();
     private JButton startButton = new JButton();
-    
-    private CardStack cardStack = new CardStack();
-
-    /**
-     * A class handle socket read/write, used in thread
-     */
-    private class ConnectionThread extends Thread {
-
-        private Socket socket;
-        private BufferedReader reader;
-        private PrintWriter writer;
-
-        /**
-         * Initialize with a specific socket
-         *
-         * @param socket socket to handle input/output
-         */
-        public ConnectionThread(Socket socket) {
-            this.socket = socket;
-            try {
-                this.writer = new PrintWriter(new OutputStreamWriter(this.socket.getOutputStream()));
-                this.reader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));      
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        /**
-         * SendMessage to socket
-         *
-         * @param message message to send
-         */
-        public void sendMessage(String message) {
-            writer.println(message);
-            writer.flush();
-        }
-
-        /**
-         * Override interface Runnable
-         */
-        @Override
-        public void run() {
-            try {
-                String name = reader.readLine();
-                String intent = reader.readLine();
-                if (name == null || intent == null) {
-                    return;
-                }
-                textArea.append(name + " wants to " + intent + ".\n");
-                usernameToIntent.put(name, intent);
-                threadToUsername.put(this, name);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-    }
 
     /**
      * Initialize server with a port to listen
      *
      * @param port port to listen
      */
-    Server(int port) {
+    private Server(int port) {
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.setLayout(null);
@@ -118,31 +48,21 @@ public class Server extends JFrame {
         startButton.setText("Start");
         startButton.setBounds(10, WINDOW_HEIGHT - 80, WINDOW_WIDTH - 2 * 10 - 5, 40);
         startButton.addActionListener(event -> {
-            startButton.setEnabled(false);
-            try {
-                serverSocket.close();
-            } catch (Exception e) {
-                e.printStackTrace();
+            if (sockets.isEmpty()) {
+                JPanel jPanel = new JPanel();
+                jPanel.add(new JLabel("No client connected!"));
+                JOptionPane.showConfirmDialog(null, jPanel, "Error", JOptionPane.DEFAULT_OPTION);
+                return;
             }
-            for (ConnectionThread th : connections) {
-                String string = "initialplayer " + threadToUsername.get(th) + " " + usernameToIntent.get(threadToUsername.get(th));
-                broadCast(string);
-            }
-            broadCast("start");
-            // TODO: deal the cards to all players
-            /*for (ConnectionThread th : connections) {
-                for(int i=0 ; i<3 ; i++) {
-                
-                }
-            }*/
-            
-            // TODO: ask each of the users to move.
+            games.add(new ServerUtility(sockets, this));
+            sockets.clear();
         });
         this.add(startButton);
 
         try {
             serverSocket = new ServerSocket(port);
-            textArea.append(date.toString() + "\n" + "Server starts listening on port " + port + ".\n");
+            Date date = new Date();
+            appendMessage(date.toString() + "\n" + "Server starts listening on port " + port + ".\n");
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(0);
@@ -152,25 +72,32 @@ public class Server extends JFrame {
     }
 
     /**
+     * Append message at the end of textfield.
+     * Make it synchronized.
+     *
+     * @param message message to append
+     */
+    public synchronized void appendMessage(String message) {
+        textArea.append(message);
+    }
+
+    /**
      * Let server start to accept connection on port specified
      */
-    public void startAcceptConnection() {
+    private void startAcceptConnection() {
         while (!serverSocket.isClosed()) {
             try {
                 Socket connSock = serverSocket.accept();
                 // Show message on server.
-                textArea.append("Connection established.\n");
+                appendMessage("Connection established.\n");
                 StringBuilder builder = new StringBuilder("Player ");
                 builder.append(connectionCount + 1).append("'s host name: ").append(connSock.getInetAddress().getHostName()).append("\n");
-                textArea.append(builder.toString());
+                appendMessage(builder.toString());
                 builder = new StringBuilder("Player ");
                 builder.append(connectionCount + 1).append("'s IP address: ").append(connSock.getInetAddress().getHostAddress()).append("\n");
-                textArea.append(builder.toString());
+                appendMessage(builder.toString());
                 connectionCount++;
-                // Create a new thread and add it to list.
-                ConnectionThread connectionThread = new ConnectionThread(connSock);
-                connectionThread.start();
-                connections.add(connectionThread);
+                sockets.add(connSock);
             } catch (SocketException e) {
                 System.out.println("Server socket closed.");
                 break;
@@ -179,31 +106,6 @@ public class Server extends JFrame {
                 break;
             }
         }
-    }
-
-    /**
-     * Send message to all socket connected to server
-     *
-     * @param message message to send
-     */
-    public void broadCast(String message) {
-        for (ConnectionThread thread : connections) {
-            thread.sendMessage(message);
-        }
-    }
-
-    /**
-     * Send message to all socket connected to server except the one specified in parameter
-     *
-     * @param message message to send
-     * @param except the socket which will be skipped when broadcasting
-     */
-    public void broadCastExcept(String message, ConnectionThread except) {
-        connections.forEach(connectionThread -> {
-            if (connectionThread != except) {
-                connectionThread.sendMessage(message);
-            }
-        });
     }
 
     public static void main(String[] args) {
---
+++ src/server/ServerUtility.java
@@ -0,0 +1,127 @@
+package server;
+
+import card.CardStack;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+/**
+ * This class handle all game input/output to the clients in list of sockets.
+ * Every socket has its own ConnectThread to handle its input/output.
+ */
+public class ServerUtility {
+
+    private ArrayList<ConnectionThread> connections = new ArrayList<>();
+    private HashMap<ConnectionThread, String> threadToUsername = new HashMap<>();
+    private TreeMap<String, String> usernameToIntent = new TreeMap<>();
+    private Server server;
+
+    private CardStack cardStack = new CardStack();
+
+    /**
+     * This class handle input/output to a single socket
+     */
+    private class ConnectionThread extends Thread {
+
+        private Socket socket;
+        private BufferedReader reader;
+        private PrintWriter writer;
+
+        ConnectionThread(Socket socket) {
+            this.socket = socket;
+            try {
+                this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                this.writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                String name = reader.readLine();
+                String intent = reader.readLine();
+                if (name == null || intent == null) {
+                    return;
+                }
+                server.appendMessage(name + " wants to " + intent + ".\n");
+                usernameToIntent.put(name, intent);
+                threadToUsername.put(this, name);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        /**
+         * SendMessage to socket
+         *
+         * @param message message to send
+         */
+        public void sendMessage(String message) {
+            writer.println(message);
+            writer.flush();
+        }
+
+        /**
+         * Override interface Runnable
+         */
+        @Override
+        public void run() {
+            System.out.println("Not completed thread");
+            // TODO: deal the cards to all players
+            /*for (ConnectionThread th : connections) {
+                for(int i=0 ; i<3 ; i++) {
+
+                }
+            }*/
+
+            // TODO: ask each of the users to move.
+        }
+
+    }
+
+    /**
+     * Initialize with a list of sockets
+     *
+     * @param sockets a list of sockets who will play together
+     * @param server server to call appendMessage() to show message on TextField
+     */
+    public ServerUtility(ArrayList<Socket> sockets, Server server) {
+        this.server = server;
+        sockets.forEach(socket -> connections.add(new ConnectionThread(socket)));
+        connections.forEach(Thread::start);
+        for (ConnectionThread connectionThread : connections) {
+            String string = "initialplayer " + threadToUsername.get(connectionThread) + " " +
+                            usernameToIntent.get(threadToUsername.get(connectionThread));
+            broadCast(string);
+        }
+        broadCast("start");
+    }
+
+    /**
+     * Send message to all socket connected to server
+     *
+     * @param message message to send
+     */
+    public void broadCast(String message) {
+        for (ConnectionThread thread : connections) {
+            thread.sendMessage(message);
+        }
+    }
+
+    /**
+     * Send message to all socket connected to server except the one specified in parameter
+     *
+     * @param message message to send
+     * @param except the socket which will be skipped when broadcasting
+     */
+    public void broadCastExcept(String message, ConnectionThread except) {
+        connections.forEach(connectionThread -> {
+            if (connectionThread != except) {
+                connectionThread.sendMessage(message);
+            }
+        });
+    }
+
+}
