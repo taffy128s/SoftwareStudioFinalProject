40d5b53c477b9ad4b5d956fc37313a26f620d20f
LittleBird <ilms49898723@gmail.com>
2016-06-05 12:01:34 +0900

    define message used during game to avoid mistyped and complex changing

---
+++ src/game/message/GameMessage.java
@@ -0,0 +1,15 @@
+package game.message;
+
+/**
+ * This class contain all string constant used during game to
+ * avoid mistype and change on copy paste. All string constant
+ * was defined as "public static final String" to used directly.
+ */
+public class GameMessage {
+
+    public static final String START = "start";
+    public static final String INITIAL_PLAYER = "initialplayer";
+    public static final String RECEIVE_CARD = "receivecard";
+    public static final String YOUR_TURN = "yourturn";
+
+}
